a10ab26bcfd9c7869452919f672b45b134cd29a8
Mconway409 <70673286+Mconway409@users.noreply.github.com>
2021-02-03 12:03:25 -0500

    Add files via upload

--- employeeTrack_db.sql
+++ employeeTrack_db.sql
@@ -4,20 +4,20 @@ CREATE database employeeTrack_db;
 USE employeeTrack_db;
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT(10)
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     name VARCHAR(30),
     department_id INT(10)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10),
